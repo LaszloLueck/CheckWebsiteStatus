869a12e1edd6d7fcc8a995d60db80e2844acae6b
Laszlo Lueck <llueck@gmail.com>
2020-11-02 19:12:00 +0100

    change implementation more async

--- CheckWebsiteStatus/Scheduler/SchedulerJob.cs
+++ CheckWebsiteStatus/Scheduler/SchedulerJob.cs
@@ -1,12 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Runtime.InteropServices.ComTypes;
 using System.Threading.Tasks;
 using System.Xml;
 using CheckWebsiteStatus.Configuration;
 using CheckWebsiteStatus.SimpleLogger;
 using HtmlAgilityPack;
+using MoreLinq;
 using Quartz;
 
 namespace CheckWebsiteStatus.Scheduler
@@ -28,19 +31,41 @@ namespace CheckWebsiteStatus.Scheduler
 
                 Logger.Log($"Retrieve the sitemap with {retList.Count} items.");
                 //Call each url in the list and print some header infos.
-                Parallel.ForEach(retList, async url => await GetResultFromUrl(url));
+
+                var parallelTasks = retList.AsParallel().Select(async url => await GetResultFromUrl(url));
+
+                var htmlNodes = (await Task.WhenAll(parallelTasks))
+                    .SelectMany(x => x)
+                    .ToList();
+
+                Logger.Log(
+                    $"Read entirely {htmlNodes.Count} sub-elements, now filter them!");
+
+
+                var cssNodes = htmlNodes
+                    .Where(node => node.Attributes.Contains("href"))
+                    .DistinctBy(node => node.Attributes["href"].Value);
+
+                var srcNodes = htmlNodes
+                    .Where(node => node.Attributes.Contains("src"))
+                    .DistinctBy(node => node.Attributes["src"].Value);
+
+                var t = Task.WhenAll(ReadAndProcessSubElements(cssNodes.Concat(srcNodes)))
+                     .ContinueWith(_ => Logger.Log("Finished Task!"));
+
+                await t;
             });
         }
 
-        private async Task GetResultFromUrl(string url)
+        private async Task<IEnumerable<HtmlNode>> GetResultFromUrl(string url)
         {
-            await Task.Run(async () =>
+            return await Task.Run(async () =>
             {
                 var response = await GetResponseFromUri(url,
                     "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
                 var headers = response.Headers;
                 var status = response.StatusCode;
-                if (status != HttpStatusCode.OK) return true;
+                if (status != HttpStatusCode.OK) return new List<HtmlNode>();
 
 
                 var doc = new HtmlDocument();
@@ -59,16 +84,14 @@ namespace CheckWebsiteStatus.Scheduler
                 var elementsImg = documentDescendants
                     .Where(node => node.Name.ToLower().TrimEnd().TrimStart() == "img")
                     .Where(node => node.Attributes.Contains("src"))
-                    .Where(node => node.Attributes["src"].Value.StartsWith("https://www.dimatec.de"))
-                    .ToList();
+                    .Where(node => node.Attributes["src"].Value.StartsWith("https://www.dimatec.de"));
 
                 var elementsJs = documentDescendants
                     .Where(node => node.Name.ToLower().TrimEnd().TrimStart() == "script")
                     .Where(node => node.Attributes.Contains("src"))
                     .Where(node => node.Attributes.Contains("type"))
                     .Where(node => node.Attributes["type"].Value.Contains("javascript"))
-                    .Where(node => node.Attributes["src"].Value.StartsWith("https://www.dimatec.de"))
-                    .ToList();
+                    .Where(node => node.Attributes["src"].Value.StartsWith("https://www.dimatec.de"));
 
 
                 var elementsCss = documentDescendants
@@ -76,72 +99,73 @@ namespace CheckWebsiteStatus.Scheduler
                     .Where(node => node.Attributes.Contains("href"))
                     .Where(node => node.Attributes.Contains("rel"))
                     .Where(node => node.Attributes["rel"].Value == "stylesheet")
-                    .Where(node => node.Attributes["href"].Value.StartsWith("https://www.dimatec.de"))
-                    .ToList();
+                    .Where(node => node.Attributes["href"].Value.StartsWith("https://www.dimatec.de"));
 
-
-                var htmlNodes = (elementsImg.Concat(elementsJs).Concat(elementsCss)).ToList();
-
-                Logger.Log(
-                    $"[url]:{url} [i]:{elementsImg.Count} [c]:{elementsCss.Count} [j]:{elementsJs.Count}");
-
-                foreach (var element in htmlNodes)
-                {
-                    switch (element.Name.ToLower().TrimEnd().TrimStart())
-                    {
-                        case "img":
-                            var imgUrl = element.Attributes["src"].Value;
-                            const string headerAcceptImages = "image/avif,image/webp,image/apng,image/*,*/*;q=0.8";
-                            var responseImage = await GetResponseFromUri(imgUrl, headerAcceptImages);
-                            if (responseImage.StatusCode == HttpStatusCode.OK)
-                            {
-                                var imgLength = getImageSize(responseImage.GetResponseStream());
-                                Logger.Log(
-                                    $"GET {imgUrl} : S {responseImage.StatusCode.ToString()} : C {responseImage.Headers["cf-cache-status"]} : L {imgLength}");
-                            }
-
-                            responseImage.Close();
-                            responseImage.Dispose();
-                            break;
-                        case "link":
-                            var cssUrl = element.Attributes["href"].Value;
-                            const string headerAcceptCss = "text/css,*/*;q=0.1";
-                            var responseCss = await GetResponseFromUri(cssUrl, headerAcceptCss);
-                            if (responseCss.StatusCode == HttpStatusCode.OK)
-                            {
-                                var cssLength = getTextSize(responseCss.GetResponseStream());
-                                Logger.Log(
-                                    $"GET {cssUrl} : S {responseCss.StatusCode.ToString()} : C {responseCss.Headers["cf-cache-status"]} : L {cssLength}");
-                            }
-
-                            responseCss.Close();
-                            responseCss.Dispose();
-                            break;
-                        case "script":
-                            var jsUrl = element.Attributes["src"].Value;
-                            const string headerAcceptJs = "*/*";
-                            var responseJs = await GetResponseFromUri(jsUrl, headerAcceptJs);
-                            if (responseJs.StatusCode == HttpStatusCode.OK)
-                            {
-                                var jsLength = getTextSize(responseJs.GetResponseStream());
-                                Logger.Log(
-                                    $"GET {jsUrl} : S {responseJs.StatusCode.ToString()} : C {responseJs.Headers["cf-cache-status"]} : L {jsLength}");
-                            }
-
-                            responseJs.Close();
-                            responseJs.Dispose();
-                            break;
-                        default:
-                            Logger.Log($"Unidentified element type <{element.Name}> found");
-                            break;
-                    }
-                }
-
-
-                return true;
+                return new List<IEnumerable<HtmlNode>> {elementsImg, elementsCss, elementsJs}.SelectMany(x => x);
             });
         }
-        
+
+        private IEnumerable<Task> ReadAndProcessSubElements(IEnumerable<HtmlNode> htmlNodes)
+        {
+            var nodes = htmlNodes.ToList();
+
+            Logger.Log($"Process {nodes.Count()} sub-elements");
+            const string headerAcceptImages = "image/avif,image/webp,image/apng,image/*,*/*;q=0.8";
+            const string headerAcceptJs = "*/*";
+            const string headerAcceptCss = "text/css,*/*;q=0.1";
+
+
+            var tl = nodes.Select(htmlNode =>
+            {
+                return htmlNode.Name.ToLower() switch
+                {
+                    "img" => GetResponseForSubElement(htmlNode.Attributes["src"].Value, headerAcceptImages,
+                        ElementType.Binary),
+                    "link" => GetResponseForSubElement(htmlNode.Attributes["href"].Value, headerAcceptCss,
+                        ElementType.Text),
+                    "script" => GetResponseForSubElement(htmlNode.Attributes["scr"].Value, headerAcceptJs,
+                        ElementType.Text),
+                    _ => Task.Run(() => { })
+                };
+            });
+            return tl;
+        }
+
+        private enum ElementType
+        {
+            Text,
+            Binary
+        }
+
+        private async Task GetResponseForSubElement(string url, string acceptHeader, ElementType elementType)
+        {
+            await Task.WhenAll(Task.Run(async () =>
+            {
+                var response = await GetResponseFromUri(url, acceptHeader);
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    var elementSize = elementType switch
+                    {
+                        ElementType.Text => getTextSize(response.GetResponseStream()),
+                        ElementType.Binary => getImageSize(response.GetResponseStream()),
+                        _ => 0L
+                    };
+
+                    Logger.Log(
+                        $"GET {url} : S {response.StatusCode.ToString()} : C {response.Headers["cf-cache-status"]} : L {elementSize}");
+                }
+                else
+                {
+                    Logger.Log(
+                        $"Could not get a valid ReturnCode from Element <{url}>. ReturnCode is {response.StatusCode}");
+                }
+
+                response.Close();
+                response.Dispose();
+            }));
+        }
+
+
         private long getImageSize(Stream responseStream)
         {
             var readStream = new BinaryReader(responseStream);
@@ -149,7 +173,7 @@ namespace CheckWebsiteStatus.Scheduler
             var lnBuffer = readStream.ReadBytes(1024);
             while (lnBuffer.Length > 0)
             {
-                ms.Write(lnBuffer,0,lnBuffer.Length);
+                ms.Write(lnBuffer, 0, lnBuffer.Length);
                 lnBuffer = readStream.ReadBytes(1024);
             }
 
@@ -163,7 +187,6 @@ namespace CheckWebsiteStatus.Scheduler
             return readStream.ReadToEnd().Length;
         }
 
-        
 
         private async Task<HttpWebResponse> GetResponseFromUri(string uri, string acceptHeader)
         {
